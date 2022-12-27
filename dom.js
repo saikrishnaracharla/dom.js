56a826cf542ae3a62dac979a47a9af7ad44f09fc
Racharla saikrishna <115521007+saikrishnaracharla@users.noreply.github.com>
2022-12-27 09:17:06 +0530

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,32 @@
+function showName(){
+    const nameTag = document.getElementById("name")
+    nameTag.innerHTML= "sai krishna"
+}
+function hideName(){
+    const nameTag = document.getElementById("name")
+    nameTag.innerHTML= ""
+}
+function showAge(){
+    const nameTag = document.getElementById("age")
+    nameTag.innerHTML= "18"
+}
+function hideAge(){
+    const nameTag = document.getElementById("age")
+    nameTag.innerHTML= ""
+}
+function showGender(){
+    const nameTag = document.getElementById("gender")
+    nameTag.innerHTML= "Male"
+}
+function hideGender(){
+    const nameTag = document.getElementById("gender")
+    nameTag.innerHTML= ""
+}
+function showSection(){
+    const nameTag = document.getElementById("Section")
+    nameTag.innerHTML= "CSE KOC27"
+}
+function hideSection(){
+    const nameTag = document.getElementById("Section")
+    nameTag.innerHTML= ""
+}
